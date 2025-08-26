cef4fdcff85b8923b6cf03c4c7e56a4136fd687d
adresa97 <adriresa97@gmail.com>
2025-08-26 19:41:59 +0200

    Cats change outfit according to web event

--- Assets/Scripts/Cat/CatWardrobe.cs
+++ Assets/Scripts/Cat/CatWardrobe.cs
@@ -42,17 +42,20 @@ public class CatWardrobe : MonoBehaviour
         }
         if (shirt.main.sprite != newShirt.main)
         {
+            Debug.Log("Recibí una camiseta nueva");
             shirt.main.sprite = newShirt.main;
             shirt.leftArm.sprite = newShirt.leftArm;
             shirt.rightArm.sprite = newShirt.rightArm;
             if (newShirt.main == null)
             {
+                Debug.Log("La camiseta es ir desnudo");
                 shirt.main.enabled = false;
                 shirt.leftArm.enabled = false;
                 shirt.rightArm.enabled = false;
             }
-            else if (!headAccesory.enabled)
+            else if (!shirt.main.enabled)
             {
+                Debug.Log("Ya basta de ir denudo por la vida");
                 shirt.main.enabled = true;
                 shirt.leftArm.enabled = true;
                 shirt.rightArm.enabled = true;
@@ -67,7 +70,7 @@ public class CatWardrobe : MonoBehaviour
                 shoes.leftShoe.enabled = false;
                 shoes.rightShoe.enabled = false;
             }
-            else if (!headAccesory.enabled)
+            else if (!shoes.leftShoe.enabled)
             {
                 shoes.leftShoe.enabled = true;
                 shoes.rightShoe.enabled = true;
